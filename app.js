7dd69d68027f7f93857949fbf007d6879d09a6eb
Abe <nurman.abedi@dana.id>
2021-09-02 13:56:47 +0700

    intial push

---
+++ app.js
@@ -0,0 +1,121 @@
+import Fastify from "fastify";
+import dotenv from "dotenv";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+dotenv.config();
+
+import Client from "@notionhq/client";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const fastify = Fastify({ logger: true });
+export default async function (fastify, opts) {
+  fastify.register(import("fastify-static"), {
+    root: join(__dirname, "public"),
+    prefix: "/public/" // optional: default '/'
+  });
+
+  const notion = new Client.Client({
+    auth: process.env.NOTION_TOKEN
+  });
+
+  fastify.get("/", async function (request, reply) {
+    return reply.sendFile("index.html"); // serving path.join(__dirname, 'public', 'myHtml.html') directly
+  });
+
+  fastify.get("/api", async function (request, reply) {
+    const quotes = await notion.databases.query({
+      database_id: process.env.DB
+    });
+
+    const map = quotes.results.map((val) => {
+      return { title: val.properties.Name.title[0].text.content };
+    });
+
+    return map;
+  });
+
+  fastify.get("/api/random", async function (request, reply) {
+    //   const listUsersResponse = await notion.users.list();
+    //   console.log(listUsersResponse);
+    const quotes = await notion.databases.query({
+      database_id: process.env.DB
+    });
+    const length = quotes.results.length;
+    const random = Math.floor(Math.random() * (length - 0));
+    console.log(random);
+    return quotes.results[random].properties.Name.title[0].text.content;
+    //   return quotes;
+  });
+
+  fastify.post("/team", async function (request, reply) {
+    const { employee_no, name, division, department, reports_to, learning_topic, duration } = request.body;
+
+    console.log("employee no", employee_no);
+    const create = await notion.pages.create({
+      parent: {
+        database_id: process.env.TEAMS_DB
+      },
+      properties: {
+        ID: {
+          title:[
+            {
+              text: {
+                content: employee_no
+              }
+            }
+          ]
+        },
+        "Employee Name": {
+          id: 'qQfN', 
+          type: 'rich_text',
+          rich_text: [
+            {
+              text: {
+                content: name
+              }
+            }
+          ]
+        },
+        "Division": {
+          type: 'select',
+          select: {
+            name: division,
+          }
+        },
+        "Department": {
+          type: 'select',
+          select: {
+            name: department,
+          }
+        },
+        "Reports To": {
+          id: '?;;g',
+          type: 'relation',
+          relation: [
+            {
+              id: reports_to,
+            }
+          ]
+        },
+        "Learning Topic": {
+          type: 'select',
+          select: {
+            name: learning_topic,
+          }
+        },
+        Duration: {
+          number: parseFloat(duration),
+        }
+      }
+    });
+    console.log("create", create);
+
+    return create;
+  });
+
+  fastify.listen(process.env.PORT || 4000, () => {
+    console.log("running on port " + process.env.PORT);
+  });
+}
